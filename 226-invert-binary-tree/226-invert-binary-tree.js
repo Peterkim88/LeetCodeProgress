9c851bf62417c855c95506351ea4e395f38e69e2
Peterkim88 <94996738+Peterkim88@users.noreply.github.com>
2022-08-23 20:45:34 -0400

    Time: 84 ms (58.30%), Space: 42.2 MB (71.51%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -10,33 +10,33 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-// var invertTree = function(root) {
-//     const reverseNodes = (node) => {
-//         if (node == null){
-//             return;
-//         }    
-//         reverseNodes(node.left);
-//         reverseNodes(node.right);
-//         let temp = node.left;
-//         node.left = node.right;
-//         node.right = temp;
-//     }
-//     reverseNodes(root);
-//     return root;
-// };
-
 var invertTree = function(root) {
-    let st = [root];
-    while(st.length > 0){
-        let node = st.pop();
-        if (node != null){
-            let temp = node.left;
-            node.left = node.right;
-            node.right = temp;
-            
-            st.push(node.left);
-            st.push(node.right);
-        }
+    const reverseNodes = (node) => {
+        if (node == null){
+            return;
+        }    
+        reverseNodes(node.left);
+        reverseNodes(node.right);
+        let temp = node.left;
+        node.left = node.right;
+        node.right = temp;
     }
+    reverseNodes(root);
     return root;
 };
+
+// var invertTree = function(root) {
+//     let st = [root];
+//     while(st.length > 0){
+//         let node = st.pop();
+//         if (node != null){
+//             let temp = node.left;
+//             node.left = node.right;
+//             node.right = temp;
+            
+//             st.push(node.left);
+//             st.push(node.right);
+//         }
+//     }
+//     return root;
+// };
