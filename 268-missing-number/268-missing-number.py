77540fbe2534e8f4cc7878fd826115f97515f825
Peterkim88 <94996738+Peterkim88@users.noreply.github.com>
2022-10-05 05:21:57 -0400

    Time: 7384 ms (5.01%), Space: 15.3 MB (41.82%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,9 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         high = max(nums)+1
+        missingNum = high
         
         for n in range(0, high):
             if n not in nums:
-                return n
+                missingNum = n
+                break
             
-        return high
+        return missingNum
