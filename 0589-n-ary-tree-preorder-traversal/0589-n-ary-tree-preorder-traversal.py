b1b5e964031facb8cc5b517d9e92c835d6c76b44
Peterkim88 <94996738+Peterkim88@users.noreply.github.com>
2022-10-30 11:02:27 -0400

    Time: 115 ms (28.15%), Space: 16 MB (81.69%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -8,16 +8,29 @@ class Node:
 
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
+        
+        if not root: return []
+        
+        q = deque([root])
         res = []
         
-        def dfs(node):
-            if not node: return
-            res.append(node.val)
-            
-            for c in node.children:
-                dfs(c)
-            
-        dfs(root)
-        
+        while q:
+            curr = q.popleft()
+            res.append(curr.val)
+            for c in reversed(curr.children):
+                q.appendleft(c)
+                
         return res
         
+#         res = []
+        
+#         def dfs(node):
+#             if not node: return
+#             res.append(node.val)
+            
+#             for c in node.children:
+#                 dfs(c)
+            
+#         dfs(root)
+        
+#         return res        
